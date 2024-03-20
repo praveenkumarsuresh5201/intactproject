6f0c4a67c2b9e3a25cac2efbb70e7bbbca552ddf
praveenkumar <114279329+praveenkumarsuresh5201@users.noreply.github.com>
2024-03-20 10:27:47 +0530

    Update main.tf

--- environments/main.tf
+++ environments/main.tf
@@ -1,21 +1,41 @@
 # Create the Resource Group
 resource "azurerm_resource_group" "this" {
-  name     = var.rgname
-  location = var.location
+  name                = var.rgname
+  location            = var.location
 
   tags = {
-    environment = var.env
+    environment       = var.env# Create the Resource Group
+    resource "azurerm_resource_group" "this" {
+    name              = var.rgname
+    location          = var.location
+
+ tags = {
+  environment          = var.env
+ }
+}
+#
+# Create the Databricks Workspace
+resource "azurerm_databricks_workspace" "this" {
+ name                  = var.wrkspname
+ resource_group_name   = azurerm_resource_group.this.name
+ location              = azurerm_resource_group.this.location
+ sku                   = var.sku
+
+ tags = {
+  environment           = var.env
+ }
+}
   }
 }
 
 # Create the Databricks Workspace
 resource "azurerm_databricks_workspace" "this" {
-  name                        = var.wrkspname
-  resource_group_name         = azurerm_resource_group.this.name
-  location                    = azurerm_resource_group.this.location
-  sku                         = var.sku
+  name                   = var.wrkspname
+  resource_group_name    = azurerm_resource_group.this.name
+  location               = azurerm_resource_group.this.location
+  sku                    = var.sku
 
   tags = {
-    environment = var.env
+    environment          = var.env
   }
 }
